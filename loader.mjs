334a0fc37bd7582e4d0afa17831213333a7f1272
elianiva <dicha.arkana03@gmail.com>
2021-10-21 16:55:27 +0700

    feat: add support for node >=16.12.0

--- loader.mjs
+++ loader.mjs
@@ -1,18 +1,21 @@
 import { URL, pathToFileURL, fileURLToPath } from 'url'
 import fs from 'fs'
 import { transformSync } from 'esbuild'
+import semverGte from 'semver/functions/gte.js'
 
 const baseURL = pathToFileURL(`${process.cwd()}/`).href
 const isWindows = process.platform === 'win32'
 
-const extensionsRegex = /\.(tsx?|json)$/;
+const extensionsRegex = /\.(tsx?|json)$/
 const excludeRegex = /^\w+:/
+
+const HAS_UPDATED_HOOKS = semverGte(process.versions.node, '16.12.0')
 
 export function resolve(specifier, context, defaultResolve) {
   const { parentURL = baseURL } = context
   const url = new URL(specifier, parentURL)
   if (extensionsRegex.test(url.pathname))
-    return { url: url.href }
+    return { url: url.href, format: 'module' }
 
   // ignore `data:` and `node:` prefix etc.
   if (!excludeRegex.test(specifier)) {
@@ -22,7 +25,10 @@ export function resolve(specifier, context, defaultResolve) {
       url.pathname = `${pathname}.${ext}`
       const path = fileURLToPath(url.href)
       if (fs.existsSync(path))
-        return { url: url.href }
+      return {
+        url: url.href,
+        format: extensionsRegex.test(url.pathname) && 'module'
+      }
     }
   }
 
@@ -30,26 +36,20 @@ export function resolve(specifier, context, defaultResolve) {
   return defaultResolve(specifier, context, defaultResolve)
 }
 
-export function getFormat(url, context, defaultGetFormat) {
+export function load(url, context, defaultLoad) {
   if (extensionsRegex.test(new URL(url).pathname)) {
-    return {
-      format: 'module',
-    }
-  }
+    const { format } = context
 
-  // Let Node.js handle all other URLs.
-  return defaultGetFormat(url, context, defaultGetFormat)
-}
-
-export function transformSource(source, context, defaultTransformSource) {
-  const { url, format } = context
-
-  if (extensionsRegex.test(new URL(url).pathname)) {
     let filename = url
-    if (!isWindows)
-      filename = fileURLToPath(url)
+    if (!isWindows) filename = fileURLToPath(url)
 
-    const { code: js, warnings, map: jsSourceMap } = transformSync(source.toString(), {
+    const rawSource = fs.readFileSync(new URL(url), { encoding: 'utf8' })
+
+    const {
+      code: js,
+      warnings,
+      map: jsSourceMap,
+    } = transformSync(rawSource.toString(), {
       sourcefile: filename,
       sourcemap: 'both',
       loader: new URL(url).pathname.match(extensionsRegex)[1],
@@ -65,10 +65,61 @@ export function transformSource(source, context, defaultTransformSource) {
     }
 
     return {
+      format: 'module',
       source: js,
     }
   }
 
-  // Let Node.js handle all other sources.
-  return defaultTransformSource(source, context, defaultTransformSource)
+  // Let Node.js handle all other format / sources.
+  return defaultLoad(url, context, defaultLoad)
 }
+
+export const getFormat = HAS_UPDATED_HOOKS
+  ? undefined
+  : (url, context, defaultGetFormat) => {
+      if (extensionsRegex.test(new URL(url).pathname)) {
+        return {
+          format: 'module',
+        }
+      }
+
+      // Let Node.js handle all other URLs.
+      return defaultGetFormat(url, context, defaultGetFormat)
+    }
+
+export const transformSource = HAS_UPDATED_HOOKS
+  ? undefined
+  : (source, context, defaultTransformSource) => {
+      const { url, format } = context
+
+      if (extensionsRegex.test(new URL(url).pathname)) {
+        let filename = url
+        if (!isWindows) filename = fileURLToPath(url)
+
+        const {
+          code: js,
+          warnings,
+          map: jsSourceMap,
+        } = transformSync(source.toString(), {
+          sourcefile: filename,
+          sourcemap: 'both',
+          loader: new URL(url).pathname.match(extensionsRegex)[1],
+          target: `node${process.versions.node}`,
+          format: format === 'module' ? 'esm' : 'cjs',
+        })
+
+        if (warnings && warnings.length > 0) {
+          for (const warning of warnings) {
+            console.warn(warning.location)
+            console.warn(warning.text)
+          }
+        }
+
+        return {
+          source: js,
+        }
+      }
+
+      // Let Node.js handle all other sources.
+      return defaultTransformSource(source, context, defaultTransformSource)
+    }
